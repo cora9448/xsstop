4034858cc165f991677f674abc1040eda0370fb7
!UKO <39997306+cora9448@users.noreply.github.com>
2022-11-20 21:30:29 +0900

    Update xsstop.py

--- xsstop.py
+++ xsstop.py
@@ -6,6 +6,6 @@ def prevent(string):
     string = string.replace('>', '&gt;')  # prevent insert tag
     string = string.replace('&', '&amp;')
     string = string.replace('\"', '&quot;')
-    string = string.replace('\'', 'ENT_QUOTES;')
+    string = string.replace('\'', '&#39;')
     string = re.sub("script|scr|ipt|(|)", "", string, flags=re.IGNORECASE)
     return string
